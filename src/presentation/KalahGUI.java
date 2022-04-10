b9b96cbb5b7db4e5a0a4b242bda375eae047d0ee
Camilo Fajardo <briancfajardo@gmail.com>
2022-04-10 16:04:10 -0500

    Menú medio funcional

--- src/presentation/KalahGUI.java
+++ src/presentation/KalahGUI.java
@@ -2,21 +2,51 @@ package presentation;
 
 import javax.swing.*;
 import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class KalahGUI extends JFrame {
-	JMenu menu;
-	public KalahGUI(){
+public class KalahGUI extends JFrame implements ActionListener {
+	private JMenuBar menu;
+	private JMenu archivo;
+	private JMenuItem nuevo;
+	private JMenuItem abrir;
+	private JMenuItem salvar;
+	private JMenuItem salir;
 
-		setVisible(true);
+
+	public KalahGUI(){
 		setTitle("Kalah");
 		prepareElements();
 		prepareActions();
-		menu = new JMenu("Nuevo menú");
+		setVisible(true);
 	}
-	public void prepareElements(){
+	private void prepareElements(){
 		setSize(960,540);
+		prepareElementsMenu();
+
+	}
+	private void prepareElementsMenu(){
+		menu = new JMenuBar();
+		setJMenuBar(menu);
+		archivo = new JMenu("Archivo");
+		menu.add(archivo);
+		nuevo = new JMenuItem("Nuevo");
+		abrir = new JMenuItem("Abrir");
+		salvar = new JMenuItem("Salvar");
+		salir = new JMenuItem("Salir");
+
+		nuevo.addActionListener(this);
+		abrir.addActionListener(this);
+		salvar.addActionListener(this);
+		salir.addActionListener(this);
+
+		archivo.add(nuevo);
+		archivo.add(abrir);
+		archivo.add(salvar);
+		archivo.add(salir);
+
 	}
 	private void prepareActions() {
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -35,7 +65,18 @@ public class KalahGUI extends JFrame {
 			System.exit(0);
 		}
 	}
+
 	public static void main(String[] arg){
 		KalahGUI gui = new KalahGUI();
+		gui.setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if(e.getSource() == salir){
+			close();
+		}
+
 	}
 }
