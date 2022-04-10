77dc5c2902328b6a65f994f4ca069b73c23b89bb
Camilo Fajardo <briancfajardo@gmail.com>
2022-04-10 14:28:55 -0500

    prueba

---
+++ src/presentation/KalahGUI.java
@@ -0,0 +1,40 @@
+package presentation;
+
+import javax.swing.*;
+import javax.swing.JOptionPane;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class KalahGUI extends JFrame {
+
+	public KalahGUI(){
+
+		setVisible(true);
+		setTitle("Kalah");
+		prepareElements();
+		prepareActions();
+	}
+	public void prepareElements(){
+		setSize(960,540);
+	}
+	private void prepareActions() {
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				close();
+			}
+		});
+	}
+	private void close(){
+		String opciones[] = {"Cerrar", "Cancelar"};
+		int elecccion = JOptionPane.showOptionDialog(this, "Confirme si desea salir", "Cerrar",
+		0,0,null, opciones,this);
+		if(elecccion == JOptionPane.YES_OPTION){
+			System.exit(0);
+		}
+	}
+	public static void main(String[] arg){
+		KalahGUI gui = new KalahGUI();
+	}
+}
