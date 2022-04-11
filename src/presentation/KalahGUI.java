3d71d80adeecad72727d009e7e4e61de01ed6826
Camilo Fajardo <briancfajardo@gmail.com>
2022-04-10 21:18:32 -0500

    Menú medio funcional

--- src/presentation/KalahGUI.java
+++ src/presentation/KalahGUI.java
@@ -2,18 +2,22 @@ package presentation;
 
 import javax.swing.*;
 import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 
 public class KalahGUI extends JFrame implements ActionListener {
 	private JMenuBar menu;
-	private JMenu archivo;
+	private JMenu archivoM;
 	private JMenuItem nuevo;
 	private JMenuItem abrir;
 	private JMenuItem salvar;
 	private JMenuItem salir;
+	private JFileChooser archivos;
+	private File partida;
 
 
 	public KalahGUI(){
@@ -25,14 +29,15 @@ public class KalahGUI extends JFrame implements ActionListener {
 	private void prepareElements(){
 		setSize(960,540);
 		prepareElementsMenu();
+		prepareElementsBoard();
 
 	}
 
 	private void prepareElementsMenu(){
 		menu = new JMenuBar();
 		setJMenuBar(menu);
-		archivo = new JMenu("Archivo");
-		menu.add(archivo);
+		archivoM = new JMenu("Archivo");
+		menu.add(archivoM);
 		nuevo = new JMenuItem("Nuevo");
 		abrir = new JMenuItem("Abrir");
 		salvar = new JMenuItem("Salvar");
@@ -43,10 +48,13 @@ public class KalahGUI extends JFrame implements ActionListener {
 		salvar.addActionListener(this);
 		salir.addActionListener(this);
 
-		archivo.add(nuevo);
-		archivo.add(abrir);
-		archivo.add(salvar);
-		archivo.add(salir);
+		archivoM.add(nuevo);
+		archivoM.add(abrir);
+		archivoM.add(salvar);
+		archivoM.add(salir);
+	}
+	private void prepareElementsBoard(){
+
 	}
 
 	private void prepareActions() {
@@ -66,6 +74,19 @@ public class KalahGUI extends JFrame implements ActionListener {
 			System.exit(0);
 		}
 	}
+	private void abrirArchivos(){
+		archivos = new JFileChooser();
+		archivos.showOpenDialog(this);
+		partida = archivos.getSelectedFile();
+	}
+
+	private void salvarArchivos(){
+		archivos = new JFileChooser();
+		archivos.showSaveDialog(this);
+	}
+	private void refresh(){
+
+	}
 
 	public static void main(String[] arg){
 		KalahGUI gui = new KalahGUI();
@@ -74,9 +95,19 @@ public class KalahGUI extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
+		if(e.getSource() == nuevo){
+
+		}
+		if(e.getSource() == abrir){
+			abrirArchivos();
+		}
+		if(e.getSource() == salvar){
+			salvarArchivos();
+		}
 		if(e.getSource() == salir){
 			close();
 		}
+
 
 	}
 }
