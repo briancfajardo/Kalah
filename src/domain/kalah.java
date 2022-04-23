eb633b3a426e266db4946894aa254a6b5566dc13
Camilo Fajardo <briancfajardo@gmail.com>
2022-04-22 22:50:05 -0500

    pruebas

--- src/domain/kalah.java
+++ src/domain/kalah.java
@@ -162,7 +162,7 @@ public class kalah {
 
     /**
      * Función que valida las reglas del juego de repetir turno y robar fichas
-     * Randoooooom
+     * Randoooooom1
      * @param fila
      * @param columna
      */
