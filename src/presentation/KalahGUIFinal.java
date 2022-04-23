601c53fe967357cc05e5860e0e5d9eb23765f548
Andrea Duran <andrea.duran@mail.escuelaing.edu.co>
2022-04-22 19:39:50 -0500

    accept

--- src/presentation/KalahGUIFinal.java
+++ src/presentation/KalahGUIFinal.java
@@ -13,48 +13,86 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import domain.kalah;
 
 public class KalahGUIFinal extends JFrame implements ActionListener {
 
     private int ancho = 960;
     private int alto = 540;
-    private Fondo fondo = new Fondo();
+    private KalahGUIFinal.Fondo fondo = new KalahGUIFinal.Fondo();
     private Button aceptar;
 
     private Button reiniciar;
-    private kalah kalah;
+    private int jug1;
+    private int jug2;
 
-    public KalahGUIFinal(kalah k){
+    private JMenuBar menu;
+    private JMenu archivoM;
+    private JMenuItem nuevo;
+    private JMenuItem abrir;
+    private JMenuItem salvar;
+    private JMenuItem salir;
+    private JFileChooser archivos;
+    private File partida;
+
+    public KalahGUIFinal(int jug1, int jug2){
         this.setContentPane(fondo);
         setTitle("Kalah");
-        prepareElements();
+        prepareElements(jug1, jug2);
         prepareActions();
         setVisible(true);
-        this.kalah = k;
-
+        this.jug1 = jug1;
+        this.jug2 = jug2;
     }
 
-    private void prepareElements(){
+    private void prepareElements(int jug1, int jug2){
         setSize(ancho,alto);
-        prepareElementsFinal();
+        prepareElementsMenu();
+        prepareElementsFinal(jug1, jug2);
 
     }
 
-    private void prepareElementsFinal(){
+    private void prepareElementsMenu(){
+        menu = new JMenuBar();
+        setJMenuBar(menu);
+        archivoM = new JMenu("Archivo");
+
+        archivoM.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        menu.add(archivoM);
+        nuevo = new JMenuItem("Nuevo");
+        abrir = new JMenuItem("Abrir");
+        salvar = new JMenuItem("Salvar");
+        salir = new JMenuItem("Salir");
+
+        nuevo.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        abrir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        salvar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        salir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        nuevo.addActionListener(this);
+        abrir.addActionListener(this);
+        salvar.addActionListener(this);
+        salir.addActionListener(this);
+
+        archivoM.add(nuevo);
+        archivoM.add(abrir);
+        archivoM.add(salvar);
+        archivoM.add(salir);
+
+    }
+
+    private void prepareElementsFinal(int jug1, int jug2){
         int ganador = 1;
         int perdedor = 2;
-        int semGanador = 0;
-        int semPerdedor = 0;
+        int semGanador = jug1;
+        int semPerdedor = jug2;
 
-        if (kalah.getContJ1() > kalah.getContJ2()){
-            semGanador = kalah.getContJ1();
-            semPerdedor = kalah.getContJ2();
-        }else{
+
+        if (jug1 < jug2){
             ganador = 2;
             perdedor = 1;
-            semGanador = kalah.getContJ2();
-            semPerdedor = kalah.getContJ1();
+            semGanador = jug2;
+            semPerdedor = jug1;
         }
 
         JLabel mensaje = new JLabel("<html><center>El juego ha acabado, <br> " +
@@ -90,6 +128,32 @@ public class KalahGUIFinal extends JFrame implements ActionListener {
 
     }
 
+    private void abrirArchivos(){
+
+        archivos = new JFileChooser();
+        archivos.showOpenDialog(this);
+        try {
+            partida = archivos.getSelectedFile();
+            String nombre = partida.getName();
+            JOptionPane.showMessageDialog(this,"El elemento está en construcción, se está abriendo un archivo\n" + nombre,"Abrir",
+                    1,null);
+        }catch (Exception e){
+
+        }
+
+    }
+
+    private void salvarArchivos(){
+        archivos = new JFileChooser();
+        archivos.showSaveDialog(this);
+        String nombre = archivos.getSelectedFile()+"";
+        if (!nombre.equals(null+"")){
+            JOptionPane.showMessageDialog(this,"El elemento está en construcción, se está guardando un archivo\n" + nombre,"Guardar",
+                    1,null);
+        }
+
+    }
+
     private void prepareActions() {
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
@@ -117,7 +181,29 @@ public class KalahGUIFinal extends JFrame implements ActionListener {
             game.setResizable(false);
             game.setLocationRelativeTo(null);
             dispose();
+
+        }if(e.getSource() == nuevo){
+            KalahGUIGame game = new KalahGUIGame();
+            game.setVisible(true);
+            game.setResizable(false);
+            game.setLocationRelativeTo(null);
+            dispose();
         }
+        if(e.getSource() == abrir){
+            abrirArchivos();
+        }
+        if(e.getSource() == salvar){
+            salvarArchivos();
+        }
+        if(e.getSource() == salir){
+            close();
+        }
+    }
+
+    public static void main(String[] arg){
+        KalahGUIFinal gui = new KalahGUIFinal(4,5);
+        gui.setResizable(false);
+        gui.setLocationRelativeTo(null);
     }
 
     class Fondo extends JPanel{
